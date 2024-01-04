8b03c6c90d66dba4bf9120dfdcd4de186e6ccae9
Do Van Thanh <thanhhoachannhan@gmail.com>
2024-01-04 11:07:47 +0700

    up

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py makemigrations
+python manage.py makemigrations core
 python manage.py migrate
 
