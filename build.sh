01b07c28c2d569a10a0e2431429a8b1204cee3af
Do Van Thanh <thanhhoachannhan@gmail.com>
2024-01-04 15:02:08 +0700

    up

--- build.sh
+++ build.sh
@@ -1,6 +1,25 @@
+pip install --upgrade pip
 pip install django==5.0
 pip install pillow==10.0
 pip install gunicorn==20.0
 
 django-admin startproject project .
+python3 manage.py startapp core
+
+echo "import os
+DEBUG=True
+ALLOWED_HOST=['*']
+INSTALLED_APPS += ['core']
+AUTH_PASSWORD_VALIDATORS = []
+MEDIA_URL = '/uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+" >> project/settings.py
+
+python3 manage.py makemigrations core
+python3 manage.py migrate
+python3 manage.py shell -c "from django.contrib.auth import get_user_model;
+get_user_model().objects.filter(username='admin').exists() or get_user_model().objects.create_superuser('admin', 'admin@admin.com', 'admin');"
 
