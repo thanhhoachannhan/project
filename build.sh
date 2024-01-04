788d6e578d71c48839b518f53c501de2549d0f95
Do Van Thanh <thanhhoachannhan@gmail.com>
2024-01-04 16:29:10 +0700

    up

--- build.sh
+++ build.sh
@@ -1,17 +1,13 @@
-sudo apt-get update -y
-sudo apt-get install python3-pip -y
 pip install --upgrade pip
 pip install django==5.0
 pip install pillow==10.0
 pip install gunicorn==20.0
 
-export PATH="$PATH:/home/thanh/.local/bin"
-source ~/.bashrc
-
 django-admin startproject project .
 python3 manage.py startapp core
 
-echo "import os
+echo "##################################################
+import os
 DEBUG=True
 ALLOWED_HOST=['*']
 INSTALLED_APPS += ['core']
@@ -22,6 +18,25 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 " >> project/settings.py
+
+echo "##################################################
+from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+
+from core.views import *
+
+urlpatterns += [
+	path('i18n/', include('django.conf.urls.i18n')),
+]
+urlpatterns += i18n_patterns(
+	
+	# prefix_default_language = False
+)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settins.MEDIA_ROOT)
+" >> project/urls.py
 
 python3 manage.py makemigrations core
 python3 manage.py migrate
