40326d308342f4c2455ab7a8d0914f4def91f05a
Do Van Thanh <thanhhoachannhan@gmail.com>
2024-01-04 08:18:49 +0700

    up

--- project/settings.py
+++ project/settings.py
@@ -121,3 +121,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+##################################################
+
+import os
+ALLOWED_HOSTS = ['*']
+
