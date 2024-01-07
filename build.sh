86f6732d167d4f8b5c5357344417877d28dff8c8
Do Van Thanh <thanhhoachannhan@gmail.com>
2024-01-07 14:39:23 +0700

    up

--- build.sh
+++ build.sh
@@ -40,7 +40,7 @@ urlpatterns += i18n_patterns(
 )
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-" >> project/urls.py
+" > project/urls.py
 
 echo "
 from django.db import models
