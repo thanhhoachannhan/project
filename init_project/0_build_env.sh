530fca1c26cab157c599b7ed567ca02e95efc161
thanhhoachannhan <155319976+thanhhoachannhan@users.noreply.github.com>
2024-06-29 10:35:35 +0700

    up

--- init_project/0_build_env.sh
+++ init_project/0_build_env.sh
@@ -1,5 +1,5 @@
 echo "[INFO] - env.build"
-pip install --upgrade pip
-pip install django==5.0
-pip install pillow==10.0
-pip install gunicorn==20.0
+pip install --upgrade pip -q
+pip install django==5.0 -q
+pip install pillow==10.0 -q
+pip install gunicorn==20.0 -q
