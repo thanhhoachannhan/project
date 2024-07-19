b01d86c07ca90d202f96041b7829caf3f9501b85
thanhhoachannhan <155319976+thanhhoachannhan@users.noreply.github.com>
2024-07-19 14:05:44 +0700

    up

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 echo "################################################## == Environment"
 # ================================================== #
 echo "[SETUP] - env.build"
-# pip install --upgrade pip -q
-# pip install django==5.0 -q
-# pip install pillow==10.0 -q
-# pip install gunicorn==20.0 -q
+pip install --upgrade pip -q
+pip install django==5.0 -q
+pip install pillow==10.0 -q
+pip install gunicorn==20.0 -q
 # ================================================== #
 echo "################################################## == Project Init"
 # ================================================== #
