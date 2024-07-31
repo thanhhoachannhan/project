9873646ab64c964e2fa9c37a69daae6eb1c34128
thanhhoachannhan <155319976+thanhhoachannhan@users.noreply.github.com>
2024-07-31 13:45:05 +0700

    up

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+"""
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install make
+"""
+
 echo "################################################## == Environment"
 # ================================================== #
 echo "[SETUP] - env.build"
