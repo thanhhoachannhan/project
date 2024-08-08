76748a4289fa8ecdf96036b4c84bff19a277e5e4
thanhhoachannhan <155319976+thanhhoachannhan@users.noreply.github.com>
2024-08-08 08:52:14 +0700

    up

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
-"""
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install make
-"""
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# choco install make
 
 echo "################################################## == Environment"
 # ================================================== #
 echo "[SETUP] - env.build"
-# pip install --upgrade pip -q
-# pip install django==5.0 -q
-# pip install pillow==10.0 -q
-# pip install gunicorn==20.0 -q
+pip install --upgrade pip -q
+pip install django==5.0 -q
+pip install pillow==10.0 -q
+pip install gunicorn==20.0 -q
 # ================================================== #
 echo "################################################## == Project Init"
 # ================================================== #
